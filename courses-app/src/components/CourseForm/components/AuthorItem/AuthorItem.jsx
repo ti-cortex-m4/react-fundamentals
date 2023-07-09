fd17e1e40311e16e53839a1be72b92d43ec3bd73
Aliaksandr Liakh <aaa@epam.com>
2023-07-09 17:10:50 +0300

    Create authors draft

--- courses-app/src/components/CourseForm/components/AuthorItem/AuthorItem.jsx
+++ courses-app/src/components/CourseForm/components/AuthorItem/AuthorItem.jsx
@@ -3,17 +3,19 @@ import { Button } from '../../../Button/Button';
 import styles from './styles.module.css';
 
 export const AuthorItem = ({
- id,
- name,
- buttonText,
- onButtonClick,
+  id,
+  name,
+  buttonText,
+  onButtonClick,
 }) => (
-	<div key={id} className={styles.authorItem}>
-		<span>{name}</span>
-        <Button
-          type='button'
-          buttonText={buttonText}
-          onClick={() => onButtonClick(id)}
-        />
-	</div>
+  <div key={id} className={styles.authorItem}>
+    <span>{name}</span>
+    <Button
+      type='button'
+      buttonText={buttonText}
+      onClick={() => onButtonClick(id)}
+    />
+  </div>
 );
+
+export default AuthorItem;
--- courses-app/src/components/CourseForm/components/Authors/Authors.jsx
+++ courses-app/src/components/CourseForm/components/Authors/Authors.jsx
@@ -1,9 +1,7 @@
-// import { Button } from '../../../Button/Button';
 import { AuthorItem } from "../AuthorItem/AuthorItem";
-// import { classNames } from '../../../../common/utils/helpers';
 import styles from './styles.module.css';
 
-const Authors = ({
+export const Authors = ({
   authors,
   buttonText,
   onButtonClick,
@@ -11,21 +9,21 @@ const Authors = ({
 
   return (
     <div>
-        {
-          (authors.length > 0) &&
-          authors.map(({ name, id }) => (
-            <AuthorItem
-              id={id}
-              name={name}
-              buttonText={buttonText}
-              onButtonClick={() => onButtonClick(id)}
-            />
-          ))
-        }
-        {
-          (authors.length === 0) &&
-          <p className={styles.notification}>author list is empty</p>
-        }
+      {
+        (authors.length > 0) &&
+        authors.map(({ name, id }) => (
+          <AuthorItem
+            id={id}
+            name={name}
+            buttonText={buttonText}
+            onButtonClick={() => onButtonClick(id)}
+          />
+        ))
+      }
+      {
+        (authors.length === 0) &&
+        <p className={styles.notification}>author list is empty</p>
+      }
     </div>
   );
 };
