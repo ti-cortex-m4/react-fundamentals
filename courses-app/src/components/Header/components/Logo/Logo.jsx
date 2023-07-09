d56534f5c8a03e69d7ef27d3a8ea4de85d1c47e1
Aliaksandr Liakh <aaa@epam.com>
2023-07-09 11:33:07 +0300

    Add logo

--- courses-app/src/components/Header/components/Logo/Logo.jsx
+++ courses-app/src/components/Header/components/Logo/Logo.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import LogoImage from '../../../../assets/logo.png';
 import styles from './styles.module.css';
 
-export const Logo = () => <img className={styles.logo} alt='logo' />;
+export const Logo = () =>
+<img
+  src={LogoImage}
+  className={styles.logo}
+  alt='logo'
+/>;
