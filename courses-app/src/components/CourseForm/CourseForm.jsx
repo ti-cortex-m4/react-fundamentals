eabe677043fc435e632ae20d361621aa5a8dd7ba
Aliaksandr Liakh <aaa@epam.com>
2023-07-09 15:04:58 +0300

    Create course draft

--- courses-app/src/components/CourseForm/CourseForm.jsx
+++ courses-app/src/components/CourseForm/CourseForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export const CreateCourse = () => {
+export const CourseForm = () => {
 
     const handleSubmit = () => {
     };
