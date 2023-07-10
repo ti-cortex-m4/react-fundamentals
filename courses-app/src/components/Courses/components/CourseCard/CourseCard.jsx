86b94eef93b973ad9f9c0fe53b92502132fefc65
Aliaksandr Liakh <aaa@epam.com>
2023-07-10 22:52:37 +0300

    Delete course

--- courses-app/src/components/Courses/components/CourseCard/CourseCard.jsx
+++ courses-app/src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -6,9 +6,9 @@ import { fetchData } from '../../../../common/utils/fetchData';
 import { APP_URL_PATHS } from '../../../../common/constants';
 import styles from './styles.module.css';
 
-export const CourseCard = ({course, authorsIdToName}) => {
+export const CourseCard = ({ course, authorsIdToName }) => {
   const {
-	id,
+    id,
     title,
     description,
     creationDate,
@@ -23,26 +23,20 @@ export const CourseCard = ({course, authorsIdToName}) => {
   const navigate = useNavigate();
 
   const handleShowButtonClick = () => {
-	navigate(APP_URL_PATHS.courseInfo + id);
+    navigate(APP_URL_PATHS.courseInfo + id);
   };
 
   const handleUpdateButtonClick = () => {
   };
 
   const handleDeleteButtonClick = (id) => {
-  console.log('deleteCourse1'+id);
-
     const deleteCourse = async (id) => {
-      console.log('deleteCourse2'+id);
-
       const { successful, error } = await fetchData({
         method: 'DELETE',
-        url: '/courses/'+id,
+        url: '/courses/' + id,
       });
 
-      console.log('finish');
       if (!error && successful) {
-      console.log('navigate');
         navigate(APP_URL_PATHS.courses);
       }
     };
@@ -50,42 +44,42 @@ export const CourseCard = ({course, authorsIdToName}) => {
     deleteCourse(id);
   };
 
-	return (
-		<div className={styles.cardContainer}>
-			<div className={styles.cardText}>
-				<h2>{title}</h2>
-				<p>{description}</p>
-			</div>
-			<div className={styles.cardDetails}>
-				<p>
-					<b>Authors: </b>
-					<span>{formattedAuthors}</span>
-				</p>
-				<p>
-					<b>Duration: </b>
-					<span>{getCourseDuration(duration)}</span>
-				</p>
-				<p>
-					<b>Created: </b>
-					<span>{formatCreationDate(creationDate)}</span>
-				</p>
-				<div>
-					
-                  <Button
-                    buttonText='Show course'
-                    onClick={handleShowButtonClick}
-                  />
-                  <Button
-                    buttonText='Edit course'
-                    onClick={handleUpdateButtonClick}
-                  />
-                  <Button
-                    buttonText='Delete course'
-                    onClick={() => handleDeleteButtonClick(id)}
-                  />
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardText}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className={styles.cardDetails}>
+        <p>
+          <b>Authors: </b>
+          <span>{formattedAuthors}</span>
+        </p>
+        <p>
+          <b>Duration: </b>
+          <span>{getCourseDuration(duration)}</span>
+        </p>
+        <p>
+          <b>Created: </b>
+          <span>{formatCreationDate(creationDate)}</span>
+        </p>
+        <div>
 
-				</div>
-			</div>
-		</div>
-	);
+          <Button
+            buttonText='Show course'
+            onClick={handleShowButtonClick}
+          />
+          <Button
+            buttonText='Edit course'
+            onClick={handleUpdateButtonClick}
+          />
+          <Button
+            buttonText='Delete course'
+            onClick={() => handleDeleteButtonClick(id)}
+          />
+
+        </div>
+      </div>
+    </div>
+  );
 };
