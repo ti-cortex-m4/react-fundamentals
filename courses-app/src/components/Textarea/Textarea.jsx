c9416e2b29b6554599ef565ba031fe14e40c8c40
Aliaksandr Liakh <aaa@epam.com>
2023-07-10 17:53:07 +0300

    Add textarea

--- courses-app/src/components/Textarea/Textarea.jsx
+++ courses-app/src/components/Textarea/Textarea.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export const Input = ({
+export const Textarea = ({
   labelText,
   placeholderText,
   type,
@@ -15,10 +15,9 @@ export const Input = ({
   return (
     <label>
       {labelText}
-      <input
+      <textarea
         className={className}
         placeholder={placeholderText}
-        type={type}
         name={name}
         value={value}
         onChange={onChange}
