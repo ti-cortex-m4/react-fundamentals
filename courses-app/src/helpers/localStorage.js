a4015385e74736d4d6c36b5803ad6cc6470e8c99
Aliaksandr Liakh <aaa@epam.com>
2023-07-14 18:58:18 +0300

    Format

--- courses-app/src/helpers/localStorage.js
+++ courses-app/src/helpers/localStorage.js
@@ -1,18 +1,18 @@
 const AUTH_TOKEN = 'authToken';
 const USER_NAME = 'userName';
 
-/* TODO */ export const getUserFromLocalStorage = () => {
+export const getUserFromLocalStorage = () => {
   const authToken = localStorage.getItem(AUTH_TOKEN);
   const userName = localStorage.getItem(USER_NAME);
   return [authToken, userName];
 }
 
-/* TODO */ export const setUserToLocalStorage = (authToken, userName) => {
+export const setUserToLocalStorage = (authToken, userName) => {
   localStorage.setItem(AUTH_TOKEN, authToken);
   localStorage.setItem(USER_NAME, userName);
 }
 
-/* TODO */ export const removeUserFromLocalStorage = () => {
+export const removeUserFromLocalStorage = () => {
   localStorage.removeItem(AUTH_TOKEN);
   localStorage.removeItem(USER_NAME);
 }
