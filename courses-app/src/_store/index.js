f777b95864408fb542e45b0c5e3261b478054135
Aliaksandr Liakh <aaa@epam.com>
2023-07-17 20:50:23 +0300

    Redux draft

---
+++ courses-app/src/_store/index.js
@@ -0,0 +1,27 @@
+import {
+  legacy_createStore as createStore,
+  compose,
+  applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { initialAuthorsState } from './authors/reducer';
+import { initialCoursesState } from './courses/reducer';
+import { initialUserState } from './user/reducer';
+
+import rootReducer from './rootReducer';
+
+const appInitialState = {
+  courses: initialCoursesState,
+  authors: initialAuthorsState,
+  user: initialUserState,
+};
+
+const store = createStore(
+  rootReducer,
+  appInitialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
