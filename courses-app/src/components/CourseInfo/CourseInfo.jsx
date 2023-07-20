e5b1db22458af58d6f11937f95e3d8a377efd09a
Aliaksandr Liakh <aaa@epam.com>
2023-07-20 16:46:36 +0300

    Revert data

--- courses-app/src/components/CourseInfo/CourseInfo.jsx
+++ courses-app/src/components/CourseInfo/CourseInfo.jsx
@@ -5,8 +5,7 @@ import { useSelector } from 'react-redux';
 import { Button } from '../../common/Button/Button';
 import { formatCourseDuration } from '../../helpers/formatCourseDuration';
 import { formatCreationDate } from '../../helpers/formatCreationDate';
-import { fetchData } from '../../helpers/fetchData';
-import { BACKEND_PATHS, APPLICATION_PATHS } from '../../constants';
+import { APPLICATION_PATHS } from '../../constants';
 
 import { getAuthorsSelector } from '../../store/authors/selectors';
 import { getCoursesSelector } from '../../store/courses/selectors';
@@ -17,36 +16,11 @@ export const CourseInfo = () => {
   const navigate = useNavigate();
   const { courseId } = useParams();
 
-//   const [course, setCourse] = useState({
-//     id: '',
-//     creationDate: '',
-//     description: '',
-//     duration: 0,
-//     title: '',
-//     authors: [],
-//   });
-
   const allAuthors = useSelector(getAuthorsSelector);
   const allCourses = useSelector(getCoursesSelector);
 
   const authorIdsToNames = new Map(allAuthors.map(author => [author.id, author.name]));
-
   const course = allCourses.find((course) => course.id === courseId);
-
-//   useEffect(() => {
-//     const getCourseInfo = async () => {
-//       const { response, error } = await fetchData({
-//         method: 'GET',
-//         url: BACKEND_PATHS.courseInfo + courseId, // TODO
-//       });
-//
-//       if (!error && response.successful) {
-//         setCourse(response.result);
-//       }
-//     };
-//
-//     getCourseInfo();
-//   }, [courseId]);
 
   const handleBackButtonClick = () => {
     navigate(APPLICATION_PATHS.courses);
@@ -69,11 +43,11 @@ export const CourseInfo = () => {
           </p>
           <p>
             <b>Duration: </b>
-            {course.duration ? formatCourseDuration(course.duration) : ''}
+            {formatCourseDuration(course.duration)}
           </p>
           <p>
             <b>Created: </b>
-            {course.creationDate ? formatCreationDate(course.creationDate) : ''}
+            {formatCreationDate(course.creationDate)}
           </p>
           <div>
             <b>Authors</b>
