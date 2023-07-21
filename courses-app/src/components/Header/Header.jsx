903aa6b1fc35d463d23a484aed1dc31b0512e6b5
Aliaksandr Liakh <aaa@epam.com>
2023-07-21 19:51:10 +0300

    Format

--- courses-app/src/components/Header/Header.jsx
+++ courses-app/src/components/Header/Header.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Logo } from './components/Logo/Logo';
-import { Logout } from './components/Logout/Logout';
-
+import { Button } from '../../common/Button/Button';
 import { getUserNameFromLocalStorage, removeUserFromLocalStorage } from '../../helpers/localStorage';
+import { LOGIN_PATH } from '../../constants';
+
 import { getUserSelector } from '../../store/user/selectors';
+import { logoutUserThunk } from '../../store/user/thunk';
+import { logoutUserErrorAction } from '../../store/user/actions';
 
 import styles from './styles.module.css';
 
-export const Header = (/*{ isLogged, setIsLogged }*/) => {
+export const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-//   const user = useSelector(getUserSelector);
-//    console.log('Header user ' + JSON.stringify(user));
+  const user = useSelector(getUserSelector);
+    const userName = getUserNameFromLocalStorage();
+
+  useEffect(() => {
+    if (user.logoutResult === true) {
+      navigate(LOGIN_PATH);
+    }
+
+    if (user.logoutResult === false) {
+      navigate(LOGIN_PATH);
+    }
+  },
+    [
+      user.logoutResult,
+      navigate,
+      dispatch
+    ]);
+
+  const handleLogoutButtonClick = () => {
+     dispatch(logoutUserThunk());
+  };
+
 
   return (
     <div className={styles.headerContainer}>
       <Logo />
 {/*       {isLogged &&  */}
-      <Logout /*setIsLogged={setIsLogged}*/ />
+    <div className={styles.userContainer}>
+      <div className={styles.userName}>
+        {userName}
+      </div>
+      <Button
+        buttonText='Logout'
+        onClick={handleLogoutButtonClick}
+      />
+    </div>
 {/*       } */}
     </div>
   );
