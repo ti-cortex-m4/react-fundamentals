9e43f8cb8e03f9d35660be9f388b1363075e406b
Aliaksandr Liakh <aaa@epam.com>
2023-07-21 14:15:30 +0300

    Implement logout

--- courses-app/src/components/Header/components/Logout/Logout.jsx
+++ courses-app/src/components/Header/components/Logout/Logout.jsx
@@ -1,18 +1,50 @@
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Button } from '../../../../common/Button/Button';
 import { getUserNameFromLocalStorage, removeUserFromLocalStorage } from '../../../../helpers/localStorage';
 import { LOGIN_PATH } from '../../../../constants';
 
+import { getUserSelector } from '../../store/user/selectors';
+import { logoutUserThunk } from '../../store/user/thunk';
+import { loginUserErrorAction } from '../../store/user/actions';
+
 import styles from './styles.module.css';
 
 export const Logout = ({ setIsLogged }) => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const userName = getUserNameFromLocalStorage();
 
+  useEffect(() => {
+    if (user.actionResult === true) {
+      //setFormData(initialFormData);
+      setIsLogged(true);
+      //setFormValid(true);
+
+      navigate(COURSES_PATH);
+    }
+
+    if (user.actionResult === false) {
+      setFormValid(false);
+      alert('Login error: ' + user.actionError);
+
+      dispatch(loginUserErrorAction({
+        actionResult: null,
+        actionError: null
+      }));
+    }
+  },
+    [
+      user.actionResult,
+      user.actionError,
+      navigate,
+      dispatch
+    ]);
+
   const handleLogoutButtonClick = () => {
-//     dispatch(logoutUser());
+     dispatch(logoutUserThunk());
 //     localStorage.removeItem('user');
 //     navigate('/login');
 
